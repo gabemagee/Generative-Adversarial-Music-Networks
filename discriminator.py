45928170ab56a50df285e0580873047d426d14d5
Maggab1031 <glma2016@mymail.pomona.edu>
2018-12-06 20:30:16 -0800

    update disc

--- discriminator.py
+++ discriminator.py
@@ -1,5 +1,46 @@
 import torchaudio
+import os
+import torch
 
-audio_tensor, sample_rate = torchaudio.load('HEAT.wav')
+os.chdir(os.getcwd()+"/post/positive/")
+audio_tensor, sample_rate = torchaudio.load('1.wav')
 sz = audio_tensor.size()
 print(sz,sample_rate)
+print("-----")
+audio_tensor, sample_rate = torchaudio.load('2.wav')
+sz = audio_tensor.size()
+print(sz,sample_rate)
+
+
+"""
+things to think about 
+
+1. what ratio of samples should go in training versus validation?
+
+2. what ratio of training set should be negative/positive
+
+3. Model architecture?
+
+ - tensor ->
+
+4. 
+
+"""
+
+input_dimension = 15000000
+output_dimension = 1
+model=torch.nn.Sequential(torch.nn.Linear(input_dimension,output_dimension))
+
+class RNN_Model(torch.nn.Module):
+
+    def __init__(self):
+        pass
+
+class Linear_Model(torch.nn.Module):
+
+    learning_rate = 0.0001
+
+    def __init__(self,input_size):
+        a = torch.nn.Linear()
+        self.model = torch.nn.Linear()
+
