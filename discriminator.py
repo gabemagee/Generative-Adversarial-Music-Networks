6f44de44725973d10940ba8abf3fd8880859ccac
Maggab1031 <glma2016@mymail.pomona.edu>
2018-12-06 21:06:39 -0800

    starting in architecture

--- discriminator.py
+++ discriminator.py
@@ -2,14 +2,16 @@ import torchaudio
 import os
 import torch
 
-os.chdir(os.getcwd()+"/post/positive/")
+o = os.getcwd()
+os.chdir(o+"/post/positive/")
 audio_tensor, sample_rate = torchaudio.load('1.wav')
-sz = audio_tensor.size()
-print(sz,sample_rate)
+sz = list(audio_tensor.size())
+print(sz[0],sample_rate)
 print("-----")
 audio_tensor, sample_rate = torchaudio.load('2.wav')
 sz = audio_tensor.size()
 print(sz,sample_rate)
+os.chdir(o)
 
 
 """
@@ -38,9 +40,60 @@ class RNN_Model(torch.nn.Module):
 
 class Linear_Model(torch.nn.Module):
 
-    learning_rate = 0.0001
+
 
     def __init__(self,input_size):
-        a = torch.nn.Linear()
-        self.model = torch.nn.Linear()
+        second_layer_input = 100
+        first_layer = torch.nn.Linear(input_size,second_layer_input)
+        second_layer = torch.nn.Linear(second_layer_input,1)
+        self.model = torch.nn.Sequential(first_layer, second_layer)
+        self.learning_rate = 0.0001
+        self.sr = 10000
+        self.sz = [input_size,1]
+        self.loss_fn = torch.nn.MSELoss(size_average=True)
 
+    def evaluate(self,input_file):
+        """
+        :param input_file: the name of the file to be evaluated
+        :return:
+        """
+        tensor, sample_rate = torchaudio.load(input_file)
+
+        #check if the sample rate and size are correct
+        assert(sample_rate != self.sr)
+        assert(list(tensor.size())!= self.sz)
+
+        return self.model(tensor)
+
+    def train(self,iterations,training_set_directory):
+        os.chdir(training_set_directory)
+        subdirs = os.listdir()
+        X = torch.Tensor()
+        Y = torch.Tensor()
+
+        for t in range(iterations):
+            Yhatt = self.model(X)
+            loss = self.loss_fn(Yhatt,Y)
+            if t % 1000 == 0:
+                print(loss.item())
+            model.zero_grad()
+            loss.backward()
+            with torch.no_grad():
+                for param in model.parameters():
+                    param -= self.learning_rate * param.grad
+
+
+def directory_to_testing_set(training_set_directory):
+    os.chdir(training_set_directory)
+    subdirs = os.listdir()
+    pos = os.getcwd()+"/positive/"
+    neg = os.getcwd()+"/negative/"
+    os.chdir(pos)
+    postitive_samples = os.listdir()
+    os.chdir(neg)
+    negative_samples = os.listdir()
+    print(postitive_samples)
+    print(negative_samples)
+
+
+directory_to_testing_set(os.getcwd()+"/pre/")
