a3360d582d8e7dc2617986dc97d74d7c8bfacde9
Maggab1031 <glma2016@mymail.pomona.edu>
2018-12-16 14:31:02 -0800

    restructuring

---
+++ gan.py
@@ -0,0 +1,15 @@
+import random
+import torchaudio
+import os
+import torch
+
+
+
+def main():
+    #divide samples in training and validation set
+    #train discriminator
+    #validation set test
+    #GAN cycle
+        #train forger for x iterations - save data
+        #train discriminator for x iterations on data
+        #produce wav files
