93d285ddae6c05ea64106866bfcf92f4517c57e8
Maggab1031 <glma2016@mymail.pomona.edu>
2018-12-17 01:46:25 -0800

    dimensionality changes

--- gan.py
+++ gan.py
@@ -1,7 +1,7 @@
 import random
-#import torchaudio
+import torchaudio
 import os
-#import torch
+import torch
 #from pathlib import Path
 
 def n_random_elts(lst,n):
@@ -29,8 +29,67 @@ def zip_tuple(lst,value):
         ret.append((item,value))
     return ret
 
+def file_to_RNN_tensor(filename):
+    tensor,sample_rate = torchaudio.load(filename)
+    b = list(torch.split(tensor,sample_rate))[:-1]
+    for i in range(len(b)):
+        b[i] = torch.reshape(b[i],tuple([10000]))
+    b = torch.stack(b)
+    return b
+
+def list_of_tuples_to_tensors(lst):
+    inputs = []
+    outputs = []
+    for item in lst:
+        inputs.append(file_to_RNN_tensor(item[0]))
+        outputs.append(torch.tensor([item[1]]))
+    return inputs,outputs
+
+
+class discriminator(torch.nn.Module):
+
+    def __init__(self,input_size,hidden_size):
+        super(discriminator,self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.i2h = torch.nn.Linear(input_size+hidden_size,hidden_size)
+        self.i2o = torch.nn.Linear(input_size+hidden_size,1)
+        self.softmax = torch.nn.LogSoftmax(dim = 0)
+        self.learning_rate = 0.005
+        self.loss = torch.nn.NLLLoss()
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input,hidden),0)
+        hidden = self.i2h(combined)
+        #print(combined.size())
+        presoftmax = self.i2o(combined)
+        output = self.softmax(presoftmax)
+        print(output.size())
+        return output,hidden
+
+    def initHidden(self):
+        return torch.zeros(self.hidden_size)
+
+    def train_rnn(self,input_tensor,result_tensor):
+
+        hidden = self.initHidden()
+
+        self.zero_grad()
+
+        for i in range(input_tensor.size()[0]):
+            output, hidden = self.forward(input_tensor[i],hidden)
+        print(output.size(),result_tensor.size())
+        loss = self.loss(output,result_tensor)
+        loss.backward()
+        for p in self.parameters():
+            p.data.add_(-self.learning_rate,p.grad.data)
+
+        return output, loss.item()
+
+
 def main():
     #divide samples in training and validation set
+    hidden_size = 10000
     directory = os.getcwd()
     ratio = 0.7
     assert('post' in list(os.listdir(directory)))
@@ -44,6 +103,20 @@ def main():
     t_n, v_n = testing_and_validation(n,ratio)
     testing_set = zip_tuple(t_p,1) + zip_tuple(t_n,0)
     validation_set = zip_tuple(v_p,1) + zip_tuple(v_n,0)
+    random.shuffle(testing_set)
+    random.shuffle(validation_set)
+    testing_tensors = list_of_tuples_to_tensors(testing_set)
+    validation_tensors = list_of_tuples_to_tensors(validation_set)
+    #print(testing_tensors[0][0])
+    input_size = list(testing_tensors[0][0].size())[1]
+    disc = discriminator(input_size,hidden_size)
+    i = 0
+    for i in range(len(testing_tensors[0])):
+        output,loss = disc.train_rnn(testing_tensors[0][i],testing_tensors[1][i])
+        print(output,loss)
+
+
+
     #train discriminator
     #validation set test
     #GAN cycle
