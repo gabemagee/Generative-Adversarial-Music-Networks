ff63ec1c91236f71087d6e6e625cd95efb65c707
Gabe Magee <glma2016@pomona.edu>
2018-12-22 01:24:34 -0800

    made the github file match the jupyter notebook version run on the server

--- gan.py
+++ gan.py
@@ -1,8 +1,8 @@
 import random
-#import torchaudio
-import time
+import torchaudio
 import os
 import torch
+import time
 import numpy
 #from pathlib import Path
 
@@ -16,19 +16,8 @@ def n_random_elts(lst,n):
         del original[i]
     return sample, original
 
-def remove_non_tensors(lst):
-    lst = lst.copy()
-    i = 0
-    while i < len(lst):
-        if lst[i].split(".")[-1]!='pt':
-            del lst[i]
-        else:
-            i+=1
-    return lst
-
 def testing_and_validation(directory,ratio):
     files = list(os.listdir(directory))
-    files = remove_non_tensors(files)
     for i in range(len(files)):
         files[i] = directory+"/"+files[i]
     n = len(files)
@@ -41,33 +30,36 @@ def zip_tuple(lst,value):
     for item in lst:
         ret.append((item,value))
     return ret
-"""
+
 def file_to_RNN_tensor(filename):
+    if not os.path.isfile(filename):
+        return torch.Tensor([0]), False
     tensor,sample_rate = torchaudio.load(filename)
+    if sample_rate!=10000:
+        return torch.Tensor([0]), False
     b = list(torch.split(tensor,sample_rate))[:-1]
     for i in range(len(b)):
-        b[i] = torch.reshape(b[i],tuple([10000])).type(torch.FloatTensor)
+        b[i] = torch.reshape(b[i],tuple([10000]))
     b = torch.stack(b)
-    return b
-"""
-
+    return b,True
 
 def list_of_tuples_to_tensors(lst):
     inputs = []
     outputs = []
     for item in lst:
         inputs.append(file_to_RNN_tensor(item[0]))
-        outputs.append(torch.tensor([item[1]]).type(torch.FloatTensor))
+        outputs.append(torch.tensor([item[1]]))
     return inputs,outputs
 
 
 class discriminator(torch.nn.Module):
 
+
     def __init__(self,input_size,hidden_size,device='cpu'):
         super(discriminator,self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
-        self.learning_rate = 0.005
+        self.learning_rate = 0.01
         self.softmax = torch.nn.LogSoftmax(dim=0).to(device)
         self.i2h = torch.nn.Linear(input_size+hidden_size,hidden_size).to(device)
         self.i2o = torch.nn.Linear(input_size+hidden_size,1).to(device)
@@ -108,8 +100,7 @@ class discriminator(torch.nn.Module):
             output, hidden = self.forward(input_tensor[i], hidden)
 
         return output
-
-
+    
 class forger(torch.nn.Module):
 
     def __init__(self, input_size, hidden_size,device='cpu'):
@@ -117,10 +108,10 @@ class forger(torch.nn.Module):
         self.device = device
         self.input_size = input_size
         self.hidden_size = hidden_size
-        self.i2h = torch.nn.Sequential(torch.nn.Linear(input_size + hidden_size,100).to(self.device),torch.nn.ReLU().to(device), torch.nn.Linear(100,hidden_size).to(self.device)).to(self.device)
+        self.i2h = torch.nn.Linear(input_size + hidden_size, hidden_size).to(self.device)
         self.i2o = torch.nn.Linear(input_size + hidden_size, input_size).to(self.device)
         self.softmax = torch.nn.LogSoftmax(dim=0).to(self.device)
-        self.learning_rate = 0.005
+        self.learning_rate = 0.01
         self.loss = torch.nn.L1Loss().to(self.device)
 
     def forward(self, input, hidden):
@@ -164,20 +155,22 @@ class forger(torch.nn.Module):
     def produce_tensor(self, filename):
         hidden = self.initHidden()
         input_tensor = random_RNN_tensor(15).to(self.device)
+        l = []
         for i in range(input_tensor.size()[0]):
             output, hidden = self.forward(input_tensor[i], hidden)
-        output = torch.reshape(output, (150000, 1)).to(self.device)
-        torch.save(output, filename)
-        # torchaudio.save(filename,output,10000)
+            l.append(output)
+        output = torch.cat(l).to(self.device)
+        output = torch.reshape(output, (150000, 1)).to(torch.device('cpu'))
+        #torch.save(output, filename)
+        torchaudio.save(filename,output,10000)
 
-def random_RNN_tensor(size):
-    b=[]
-    for i in range(size):
-        b.append(torch.randint(low = -100000000,high = 100000000, size = tuple([10000])).type(torch.FloatTensor))
-    b = torch.stack(b)
-    return b
-
-"""
+def dir_to_tensors(directory):
+    files = os.listdir(directory)
+    lst = []
+    for file in files:
+        t = file_to_RNN_tensor(file)
+        lst.append(t)
+    return lst
 def dir_to_tensors(directory):
     files = os.listdir(directory)
     os.chdir(directory)
@@ -218,29 +211,13 @@ def files_to_samples():
             i +=1
             if i%1000==0:
                 print(i/j)
-"""
-
-
-def divide_into_n(lst,n):
-    m = len(lst)
-    p = int(m/n)
-    r = []
-    for i in range(0,m,p):
-        r.append(lst[i:i+p])
-    return r
-
-def load_model(filename,disc_frgr):
-    device = torch.device("cuda")
-    if disc_frgr=="discriminator":
-        a = discriminator
-    elif disc_frgr=="forger":
-        a = forger
-    else:
-        raise Exception("choose discriminator or forger")
-    model = a(*args, **kwargs)
-    model.load_state_dict(torch.load(filename))
-    model.to(device)
-
+                
+def random_RNN_tensor(size):
+    b = []
+    for i in range(size):
+        b.append(torch.randint(low = -100000000,high = 100000000, size = tuple([10000])).type(torch.FloatTensor))
+    b = torch.stack(b)
+    return b
 
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -249,18 +226,18 @@ def main():
     # divide samples in training and validation set
     hidden_size = 1000
     input_size = 10000
-    epochs = 4
+    epochs = 10
+    
     count = 0
-    frgr_training_iter = 1000
+    frgr_training_iter = 100000
     ratio_to_be_tested = .7
-    saved_model_directory = os.getcwd() + "\\models"
-    positive_samples_directory = r"C:\Users\GMagee1\Box\positive"
-    negative_samples_directory = r"C:\Users\GMagee1\Box\negative"
-    created_wav_tensors_directory = r"C:\Users\GMagee1\Box\wavs"
-    t = time.time()
+    #change this @@@
+    saved_model_directory = os.getcwd() + "/post"
+    positive_samples_directory = os.getcwd() + "/post/positive"
+    negative_samples_directory = os.getcwd() + "/post/negative"
+    created_wav_tensors_directory = os.getcwd() + "/post"
     disc = discriminator(input_size, hidden_size,device).to(device)
     frgr = forger(input_size, hidden_size,device).to(device)
-    print(time.time() - t)
     p_t, p_v = testing_and_validation(positive_samples_directory, ratio_to_be_tested)
     n_t, n_v = testing_and_validation(negative_samples_directory, ratio_to_be_tested)
     testing_set = zip_tuple(p_t, 1) + zip_tuple(n_t, 0)
@@ -269,46 +246,62 @@ def main():
     random.shuffle(validation_set)
     # training
     s = []
-    total = (epochs*len(testing_set)//2)
+    j = 0
+    total = (epochs*len(testing_set))
     for i in range(epochs):
         t = time.time()
-        j = 0
         random.shuffle(testing_set)
-        for sample, result in testing_set[:len(testing_set)//2]:
-            #print(sample)
+        for sample, result in testing_set[:len(testing_set)]:
             t0 = time.time()
-            input = torch.load(sample,map_location=device)
-            result_tensor = torch.Tensor(tuple([result])).cuda()
-            _,l = disc.train_rnn(input_tensor=input, result_tensor=result_tensor)
-            s.append(l)
-            #print("load: ",t-t0," train: ",time.time()-t)
-            j+=1
-            if j%500==0:
-                percentage = int((j/total)*100)
-                print(percentage,"% done training.",j," examples seen.")
-                print("Average Loss ",sum(s)/len(s))
-                s = []
+            input, boole = file_to_RNN_tensor(sample)
+            if boole:
+                input = input.cuda()
+                result_tensor = torch.Tensor(tuple([result])).cuda()
+                t = time.time()
+                _,l = disc.train_rnn(input_tensor=input, result_tensor=result_tensor)
+                s.append(l)
+                #print("load: ",t-t0," train: ",time.time()-t)
+                j+=1
+                if j%1000==0:
+                    percentage = int((j/total)*100)
+                    print(percentage,"% done training.",j," examples seen.")
+                    print("Average Loss ",sum(s)/len(s))
+                    s = []
         print(time.time() - t, len(testing_set))
-        torch.save(disc, saved_model_directory+"\\discriminators\\"+str(i)+".pt")
-
-
-    # validation
+        #torch.save(disc, saved_model_directory+"/discriminators/"+str(i)+".pt
     avgs = []
-    for sample, result in validation_set:
-        input = torch.load(sample).to(device)
-        o = disc.examine_forgery(input).data.cpu().numpy()[0]
-        #print(o)
-        avgs.append(numpy.abs(result - o))
+    i = 0
+    for sample, result in validation_set[:len(validation_set)//4]:
+        input,boole = file_to_RNN_tensor(sample)
+        if boole:
+            i+=1
+            print(i)
+            input = input.to(device)
+            o = disc.examine_forgery(input).data.cpu().numpy()[0]
+            #print(o)
+            avgs.append(numpy.abs(result - o))
     print("Average loss: ",sum(avgs)/len(avgs))
+    
+    
+    
+    disc = torch.load("post/discriminators/1.pt").to(device)
+    disc.eval()
+    # validation
+   
 
     for j in range(epochs):
         created = []
-        for i in range(frgr_training_iter):
-            a, _ = frgr.train_rnn(disc)
+        for i in range(1000):
+            a, losss = frgr.train_rnn(disc)
             created.append((a, 0))
-        torch.save(frgr, saved_model_directory + "\\forgers\\" + str(j) + ".pt")
+            if losss<0.5:
+                frgr.produce_tensor(os.getcwd()+"/post/_p"+str(count)+".wav")
+                count+=1
+            if i%100==0:
+                print((j/frgr_training_iter)*100,"% done training forger for epoch", j)
+        torch.save(frgr, saved_model_directory + "/forgers/" + str(j) + ".pt")
         for t in created:
-            torch.save(t,created_wav_tensors_directory+"\\"+str(count)+".pt")
+            torchaudio.save(created_wav_tensors_directory+"/"+str(count)+".wav",t,10000)
             count+=1
         random.shuffle(created)
         for sample, result in created:
@@ -316,14 +309,8 @@ def main():
             input = sample.to(device)
             result_tensor = torch.Tensor(tuple([result])).to(device)
             disc.train_rnn(input_tensor=input, result_tensor=result_tensor)
-        torch.save(disc, saved_model_directory + "\\discriminators\\" + str(i+epochs) + ".pt")
+        #torch.save(disc, saved_model_directory + "/discriminators/" + str(i+epochs) + ".pt")
         #save dicriminator here
-
-
-
-
-
-
 
 
 
